8ae72adc8ac3d28816addbf20e79595516815b8f
Akash1yadavv <akash_55@outlook.com>
2022-08-26 00:25:39 +0530

    tutorial,birthday and graduation pages

---
+++ scripts/tutorial.js
@@ -0,0 +1,39 @@
+let video_part = document.querySelectorAll(".desc_part")
+
+let previus;
+let count=0
+let buttons = document.querySelectorAll("#buttons>h2")
+
+video_part[0].style.display = 'grid';
+
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", (event) => {
+        count++
+        video_part[0].style.display = 'none';
+       
+        if(count>1){
+        video_part[previus].style.display = 'none'
+        }
+        previus=i
+        video_part[i].style.display = 'grid';
+
+        console.log(i)
+    })
+}
+
+
+// let flag = false;
+// document.getElementById("mensbtn").addEventListener("click",()=>{
+//     if(flag==false){
+//         let div = document.getElementById("popdown");
+//         div.style.display="block";
+//         flag=true;
+
+//     }
+//     else{
+//         let div = document.getElementById("popdown");
+//         div.style.display="none";
+//         flag=false;
+//     }
+// })
