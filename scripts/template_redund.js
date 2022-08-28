7544651a05870b6b0ce9f6ca4fd95d00a4acb455
indicate0 <indicate0@gmail.com>
2022-08-28 15:02:11 +0530

    some modification to template page

---
+++ scripts/template_redund.js
@@ -0,0 +1,148 @@
+let left_side=document.getElementById("left-side");
+let h1=document.createElement("h1");
+let p=document.createElement("p");
+function recommended(){
+    h1.innerText="Recommended for you";
+    p.innerText="We suggest starting with these templates.";
+    left_side.append(h1,p);
+    console.log(h1);
+}
+
+function all_templates(){
+    h1.innerText="All templates";
+    p.innerText="Browse our entire collection.";
+    left_side.append(h1,p);
+}
+
+function New(){
+    h1.innerText="NEW";
+    p.innerText="Discover the newest templates added to Animoto.";
+    console.log(h1);
+    console.log(p);
+    left_side.append(h1,p);
+}
+
+function popular(){
+    h1.innerText="Popular";
+    p.innerText="Check out our most popular templates.";
+    left_side.append(h1,p);
+}
+
+function about_us(){
+    h1.innerText="About Us";
+    p.innerText="Share how and why you got started.";
+    left_side.append(h1,p);
+}
+
+function amazon_promo(){
+    h1.innerText="Amazon Promo";
+    p.innerText="Promote and drive sales of your Amazon products using these video templates co-designed and tested by Animoto and Amazon.";
+    left_side.append(h1,p);
+}
+
+function behind_scenes(){
+    h1.innerText="Behind The Scenes";
+    p.innerText="Give your customers an inside look at your work.";
+    left_side.append(h1,p);
+}
+
+function celebrations(){
+    h1.innerText="Celebrations";
+    p.innerText="Create a video full of memories for any occasion.";
+    left_side.append(h1,p);
+}
+
+function education(){
+    h1.innerText="Education";
+    p.innerText="Easily create and share videos with your class, with parents, and beyond.";
+    left_side.append(h1,p);
+}
+
+function eventt(){
+    h1.innerText="Event";
+    p.innerText="Promote, coordinate or recap your event .";
+    left_side.append(h1,p);
+}
+
+function explainer(){
+    h1.innerText="Explainer";
+    p.innerText="Showcase your expertise by breaking down a complicated topic.";
+    left_side.append(h1,p);
+}
+
+function holiday(){
+    h1.innerText="Holiday";
+    p.innerText="Spread holiday cheer and mark the new year with a festive video.";
+    left_side.append(h1,p);
+}
+
+function list(){
+    h1.innerText="List";
+    p.innerText="Break down your message into a roundup of related information.";
+    left_side.append(h1,p);
+}
+
+function motivational(){
+    h1.innerText="Motivational";
+    p.innerText="Inspire your followers with a powerful message.";
+    left_side.append(h1,p);
+}
+
+function narrative(){
+    h1.innerText="Narrative";
+    p.innerText="Add depth to your story by taking the audience on a detailed journey.";
+    left_side.append(h1,p);
+}
+
+function product_promotion(){
+    h1.innerText="Product Promotion";
+    p.innerText="Announce a launch and highlight key product features.";
+    left_side.append(h1,p);
+}
+function real_estate(){
+    h1.innerText="Real Estate";
+    p.innerText="Show off your listings and promote your real estate business.";
+    left_side.append(h1,p);
+}
+
+function service_promotion(){
+    h1.innerText="Service Promotion";
+    p.innerText="Attract attention to your latest offerings.";
+    left_side.append(h1,p);
+}
+
+function slideshow(){
+    h1.innerText="Slideshow";
+    p.innerText="Show off your images in a simple and visually captivating way.";
+    left_side.append(h1,p);
+}
+
+function teaser(){
+    h1.innerText="Teaser";
+    p.innerText="Build anticipation for an upcoming project or a product.";
+    left_side.append(h1,p);
+}
+
+function testimoial(){
+    h1.innerText="Testimonial";
+    p.innerText="Build credibility by sharing what others are saying about you.";
+    left_side.append(h1,p);
+}
+
+function tutorial(){
+    h1.innerText="Tutorial";
+    p.innerText="Teach others how to do what you do while promoting your product .";
+    left_side.append(h1,p);
+}
+
+function valentine_day(){
+    h1.innerText="Valentine's Day";
+    p.innerText="Love is in the air! Promote your business or make a video valentine to show you care.";
+    left_side.append(h1,p);
+}
+
+function workplace(){
+    h1.innerText="Workplace";
+    p.innerText="Share company news, train teammates, and build connections with colleagues.";
+    left_side.append(h1,p);
+}
